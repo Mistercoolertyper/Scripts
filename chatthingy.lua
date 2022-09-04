59e30c3ff6acb288ad0ff982740eca23f06b96c6
Mistercoolertyper <84140706+Mistercoolertyper@users.noreply.github.com>
2022-09-04 11:06:36 +0200

    bypass function, wich works on basically any word

--- chatthingy.lua
+++ chatthingy.lua
@@ -1,44 +1,156 @@
---u can't use team chat with this, it's made to bypass roblox chat filter
+--you will not be able to use team chat or to private message people with this script, as i modify/replace some connections and i'm too lazy to make it work
 --don't expect all the bypasses to work, because roblox loves patching these
 repeat task.wait() until game:IsLoaded()
 task.wait(1)
+if getgenv().chatmod then
+    for i,v in next, getgenv().chatmod.connections do
+        v:Disconnect()
+    end
+end
+getgenv().chatmod = {
+    connections = {},
+}
+local lplr = game:GetService("Players").LocalPlayer
 
 local Chat = lplr.PlayerGui.Chat
+local ChatFrame = Chat.Frame
 local Box = Chat.Frame.ChatBarParentFrame.Frame.BoxFrame.Frame.ChatBar
-Box.Focused:connect(function()
+
+getgenv().chatmod.connections[#getgenv().chatmod.connections+1] = Box.Focused:connect(function()
     Box.Text = ""
 end)
 
 for i,v in next, getconnections(Box.FocusLost) do
-	v:Disable()
+    v:Disable()
+end
+
+local bypasschars1 = {
+    ["a"] = "🄐",
+    ["b"] = "🄑",
+    ["c"] = "🄒",
+    ["d"] = "🄓",
+    ["e"] = "🄔",
+    ["f"] = "🄕",
+    ["g"] = "🄖",
+    ["h"] = "🄗",
+    ["i"] = "🄘",
+    ["j"] = "🄙",
+    ["k"] = "🄚",
+    ["l"] = "🄛",
+    ["m"] = "🄜",
+    ["n"] = "🄝",
+    ["o"] = "🄞",
+    ["p"] = "🄟",
+    ["q"] = "🄠",
+    ["r"] = "🄡",
+    ["s"] = "🄢",
+    ["t"] = "🄣",
+    ["u"] = "🄤",
+    ["v"] = "🄥",
+    ["w"] = "🄦",
+    ["x"] = "🄧",
+    ["y"] = "🄨",
+    ["z"] = "🄩",
+}
+
+local bypasschars2 = {
+    ["a"] = "🅐",
+    ["b"] = "🅑",
+    ["c"] = "🅒",
+    ["d"] = "🅓",
+    ["e"] = "🅔",
+    ["f"] = "🅕",
+    ["g"] = "🅖",
+    ["h"] = "🅗",
+    ["i"] = "🅘",
+    ["j"] = "🅙",
+    ["k"] = "🅚",
+    ["l"] = "🅛",
+    ["m"] = "🅜",
+    ["n"] = "🅝",
+    ["o"] = "🅞",
+    ["p"] = "🅟",
+    ["q"] = "🅠",
+    ["r"] = "🅡",
+    ["s"] = "🅢",
+    ["t"] = "🅣",
+    ["u"] = "🅤",
+    ["v"] = "🅥",
+    ["w"] = "🅦",
+    ["x"] = "🅧",
+    ["y"] = "🅨",
+    ["z"] = "🅩",
+}
+
+local function bypass(word)
+    word = word:lower()
+    local newstr = ""
+    local half = math.round((#word)/2)
+    local bypass = {}
+
+    for i = 1,#word do
+        if i > half-1 then
+            bypass[#bypass+1] = word:sub(i,i):gsub(word:sub(i,i), (bypasschars1[word:sub(i,i)] or word:sub(i,i)))
+        else
+            bypass[#bypass+1] = word:sub(i,i):gsub(word:sub(i,i), (bypasschars2[word:sub(i,i)] or word:sub(i,i)))
+        end
+    end
+    for i,v in bypass do
+        newstr = newstr..v
+    end
+    return newstr
 end
 
 local bypasses = {
-    ["trollage"] = "troll\240\144\140\145age",
-    --["discord.gg/"] = "Discord .ɡɡ/",
-    --["discord."] = "Discord .",
-    ["discord"] = "Discord",
-    ["discord."] = "D\240\144\140\145\240\144\140\145\240\144\140\145i\240\144\140\145\240\144\140\145\240\144\140\145s\240\144\140\145\240\144\140\145\240\144\140\145c\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145ord .",
-    --["discord."] = "d\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145і\240\144\140\145s\240\144\140\145\240\144\140\145\240\144\140\145c\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145o\240\144\140\145r\240\144\140\145d.",
-    ["lmao"] = "lm \240\144\140\145 \205\156 \205\163ao",
-    ["lmfao"] = "lm \240\144\140\145\205\156\205\163fɑo",
-    ["LMFAO"] = "lm \240\144\140\145 \205\156 \205\163fao",
-    --["discord"] = "dd\240\144\140\145\240\144\140\145\240\144\140\145i\240\144\140\145\240\144\140\145\240\144\140\145s\240\144\140\145\240\144\140\145\240\144\140\145c\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145ord",
-    ["vape"] = "va\240\144\140\145 \205\156 \205\163pe",
-    ["LMAO"] = "LM \240\144\140\145 \205\156 \205\163AO",
-    ["fuck"] = "ff\240\144\140\145\240\144\140\145\240\144\140\145u\240\144\140\145\240\144\140\145\240\144\140\145ϲ\240\144\140\145\240\144\140\145\240\144\140\145k\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145",
-    ["fucked"] = "ff\240\144\140\145\240\144\140\145\240\144\140\145u\240\144\140\145\240\144\140\145\240\144\140\145ϲ\240\144\140\145\240\144\140\145\240\144\140\145k\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145 ed",
-    ["shit"] = "Sh\240\144\140\145 \205\156 \205\163 it",
-    ["fuckyou"] = "ff\240\144\140\145\240\144\140\145\240\144\140\145u\240\144\140\145\240\144\140\145\240\144\140\145ϲ\240\144\140\145\240\144\140\145\240\144\140\145k\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145уou",
-    ["kys"] = "ky\240\144\140\145 \205\156 \205\163s",
-    ["wtf"] = "ѡ\240\144\140\145 \205\156 \205\163t\240\144\140\145 \205\156 \205\163f",
-    ["WTF"] = "ѡ\240\144\140\145 \205\156 \205\163T\240\144\140\145 \205\156 \205\163F",
-    ["fucker"] = "ff\240\144\140\145\240\144\140\145\240\144\140\145u\240\144\140\145\240\144\140\145\240\144\140\145ϲ\240\144\140\145\240\144\140\145\240\144\140\145k\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145er",
-    ["fucking"] = "ff\240\144\140\145\240\144\140\145\240\144\140\145u\240\144\140\145\240\144\140\145\240\144\140\145ϲ\240\144\140\145\240\144\140\145\240\144\140\145k\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145\240\144\140\145ing",
+    ["fuck"] = bypass("fuck"),
+    ["fucking"] = bypass(" fuck ing"),
+    ["fucked"] = bypass(" fucked"),
+    ["nigger"] = bypass(" nigger"),
+    ["nigga"] = "nnn ii gg aa",
+    ["niggas"] = bypass(" niggas"),
+    ["niggers"] = bypass(" niggers"),
+    ["vape"] = bypass(" va pe"),
+    ["discord"] = bypass(" discord"),
+    ["kys"] = bypass(" kys"),
+    ["faggot"] = bypass(" faggot"),
+    ["lmfao"] = bypass("lmfao"),
+    ["LMFAO"] = bypass("lmfao"),
+    ["lmao"] = bypass("lmao"),
+    ["LMAO"] = bypass("lmao"),
+    ["shit"] = bypass("shit"),
+    ["cotton"] = bypass("cotton"),
+    ["cock"] = bypass("cock"),
+    ["ass"] = bypass("cock"),
+    ["tit"] = bypass("tit"),
+    ["tits"] = bypass("tits"),
+    ["wtf"] = bypass("wtf"),
+    ["WTF"] = bypass("wtf"),
+    ["trollage"] = bypass("trollage"),
 }
 
-Box.FocusLost:connect(function(enter)
+getgenv().chatmod.connections[#getgenv().chatmod.connections+1] = Box.FocusLost:connect(function(enter)
     if enter then
+        local getidentity = syn and syn.get_thread_identity or getthreadidentity
+        local setidentity = syn and syn.set_thread_identity or getthreadidentity
+        local newtext = ""
+        local found = false
+        firesignal(lplr.Chatted, "hi")
+        if Box.Text:find(".bind") then
+            local args = Box.Text:split(" ")
+            for i,v in next, shared.GuiLibrary.ObjectsThatCanBeSaved do
+                if i:lower() == args[2]:lower().."optionsbutton" then
+                    if args[3] then
+                        local oldidentity = getidentity()
+                        setidentity(8)
+                        v["Api"]["SetKeybind"]((args[3]:lower() == "none" and "" or args[3]:upper()))
+                        shared.GuiLibrary.CreateNotification("Module Bound", "Successfully Bound "..tostring(i):gsub("OptionsButton", "").."\nTo Key ".. string.upper(args[3]), 5)
+                        setidentity(oldidentity)
+                    end
+                end
+            end
+            Box.Text = 'To Chat click here or press "/" key'
+            return
+        end
         if Box.Text == "swastika" then
             spawn(function()
                 task.wait(0.5)
@@ -58,20 +170,20 @@ Box.FocusLost:connect(function(enter)
             Box.Text = 'To Chat click here or press "/" key'
             return
         end
-        if Box.Text:lower():find("fuck you") then
-            newtext = Box.Text:gsub("fuck you", bypasses["fuckyou"])
-            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(newtext, "All")
-            Box.Text = 'To Chat click here or press "/" key'
-            return
-        end
-        for i,v in pairs(bypasses) do
-            if Box.Text:find(i) then
-                found = true
-                newtext = Box.Text:gsub(i, v)
-            elseif not found then
-                newtext = Box.Text
+        local split = Box.Text:split(" ")
+        for i,v in next, bypasses do
+            for i2,v2 in next, split do
+                if v2 == i then
+                    split[i2] = v2:gsub(i,v)
+                end
             end
         end
+        if Box.Text:find(";kick default") then
+            newtext = ";kill default"
+            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(newtext, "All")
+            task.wait(1)
+        end
+        newtext = table.concat(split, " ")
         if newtext ~= "" then
             game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(newtext, "All")
         end
